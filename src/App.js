33448ef6931eae4ac1b58c4dd950d717602e516d
jaktorn <jaktopn@gmail.com>
2024-08-05 10:24:22 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from 'react'
+import'./style.css'
+
+
+
+function App(){
+  return(
+    <>
+    <h2>Develoer Thai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
+  )
+}
+export default App;

90080406ed07b0ce0efc42cd4de53ca8347e2a57
Jaktorn <156067837+Jaktorn@users.noreply.github.com>
2024-09-30 11:26:40 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import FormText from "./form-text";
 import FormSelectFile from "./form-select-file";
-import FormValidation from "./form-validation";
 
 export default function App(){
-  return <FormValidation/>
+  return <FormSelectFile/>
 }
